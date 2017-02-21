a397314d544318c0089565f35a14c1f889f775f4
Annika Keil <annika.keil95@gmail.com>
2017-02-21 11:43:08 +0100

    Build-Scripte angepasst und Wheel.jar gebildet

---
+++ exchange_component_02/src/Wheel.java
@@ -0,0 +1,50 @@
+public class Wheel {
+    private String manufacturer;
+    private String type;
+    private String id;
+    private boolean isBrakeSet = false;
+
+    private static Wheel instance = new Wheel();
+
+    public static Wheel getInstance() {
+        return instance;
+    }
+
+    public Port port;
+
+    private Wheel() {
+        port = new Port();
+    }
+
+    public String getVersion() {
+        return "Aweseom Wheel Two - Queen Edition";
+    }
+
+    public class Port implements IWheel {
+
+        public boolean setBrake() {
+            return innerSetBreak();
+        }
+
+        public boolean releaseBreak() {
+            return innerReleaseBreak();
+        }
+
+    }
+
+    private boolean innerSetBreak() {
+        if(isBrakeSet == false){
+            isBrakeSet = true;
+            return true;
+        }
+        return false;
+    }
+
+    private boolean innerReleaseBreak() {
+        if(isBrakeSet == true){
+            isBrakeSet = false;
+            return true;
+        }
+        return false;
+    }
+}
